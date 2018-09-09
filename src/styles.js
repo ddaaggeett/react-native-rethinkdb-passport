e844bb32a069c6832b4335dcfedcd446470f5ac9
ddaaggeett <david.g.daggett@gmail.com>
2018-09-09 09:59:23 -0400

    formatting

---
+++ src/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: 20,
+    },
+    avatarImage: {
+        borderRadius: 50,
+        height: 100,
+        width: 100,
+    },
+    header: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    text: {
+        textAlign: 'center',
+        color: '#333',
+        marginBottom: 5,
+    },
+    buttons: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        margin: 20,
+        marginBottom: 30,
+    },
+})
+
+export const iconStyles = {
+    borderRadius: 10,
+    iconStyle: { paddingVertical: 5 },
+}
